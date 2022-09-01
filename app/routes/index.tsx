a087fb9e307bd4afb016a931c9df248d7e9e3c9a
Lucas Westermann <lucas.westermann@gmail.com>
2022-09-01 12:13:45 -0400

    Initial commit with repro

---
+++ app/routes/index.tsx
@@ -0,0 +1,42 @@
+import { Form } from "@remix-run/react";
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <Form>
+        <select name="filter">
+          <option value="">Select filter</option>
+          <option value="all">All</option>
+          <option value="active">Active</option>
+        </select>
+        <button type="submit">Filter</button>
+      </Form>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
